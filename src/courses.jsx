25103082b0f36fa57c719d0014fef2e0503feab7
Julie Torres <julielizabeth12.tr@gmail.com>
2024-12-01 20:03:28 -0700

    Le quite y sigo en la busqueda de configuraciones css que solo complican mas las cosas

--- src/courses.jsx
+++ src/courses.jsx
@@ -91,6 +91,24 @@ function Courses() {
                 onChange={(e) => setSearchTerm(e.target.value)}
                 className="search-input"
               />
+              <div class="InputContainer">
+                <input
+                  placeholder="Buscar curso"
+                  name="text"
+                  type="text"
+                  value={searchTerm}
+                  className="search-input"
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                />
+
+                <label class="labelforsearch" for="input">
+                  <svg class="searchIcon" viewBox="0 0 512 512">
+                    <path
+                      d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"
+                    ></path>
+                  </svg>
+                </label>
+              </div>
               <button onClick={() => setShowAddForm(true)} className="add-course-button">
                 <FontAwesomeIcon icon={faPlus} /> Agregar Curso
               </button>
